0b0040f1bec9a4649de962a76d7e78240242bb27
Khaled Hazime <hazimekhaled01@gmail.com>
2023-06-27 05:47:08 -0300

    Publishing direction to /pointing_direction topic and processed image to /processed_image

--- src/posture_angle_detector.py
+++ src/posture_angle_detector.py
@@ -2,6 +2,7 @@
 import rospy
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
+from std_msgs.msg import String
 import cv2
 import mediapipe as mp
 import numpy as np
@@ -14,13 +15,18 @@ class PostureAnalyzer:
         self.bridge = CvBridge()
         self.camera_topic = rospy.get_param('~camera_topic', '/usb_cam/image_raw')
         self.camera_subscriber = rospy.Subscriber(self.camera_topic, Image, self.callback_on_new_image)
+        self.pointing_direction_publisher = rospy.Publisher('/pointing_direction', String, queue_size=10)
+        self.processed_image_publisher = rospy.Publisher('/processed_image', Image, queue_size=10)
         self.last_pointing_direction = None
 
     def callback_on_new_image(self, image_message):
         try:
             image_frame = self.bridge.imgmsg_to_cv2(image_message, desired_encoding='passthrough')
             image_frame_copy = np.copy(image_frame)
-            self.analyze_posture_in_frame(image_frame_copy)
+            processed_image = self.analyze_posture_in_frame(image_frame_copy)
+            processed_image_message = self.bridge.cv2_to_imgmsg(processed_image, encoding='bgr8')
+            self.processed_image_publisher.publish(processed_image_message)
+
         except Exception as error:
             print(error)
             return
@@ -38,8 +44,10 @@ class PostureAnalyzer:
     def publish_pointing_side(self, right_shoulder_angle, left_shoulder_angle):
         if right_shoulder_angle > 20 > left_shoulder_angle:
             self.last_pointing_direction = 'left'
+            self.pointing_direction_publisher.publish('left')
         elif right_shoulder_angle < 20 < left_shoulder_angle:
             self.last_pointing_direction = 'right'
+            self.pointing_direction_publisher.publish('right')
 
     def analyze_posture_in_frame(self, frame):
         with self.mediapipe_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
